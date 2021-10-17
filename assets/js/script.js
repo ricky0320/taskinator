aa452c97244e636d3a93c1d819c66045511ee322
Ricky Leung <ricxx0320@gmail.com>
2021-10-17 01:51:08 -0700

    added delete button to remove task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var taskIdCounter = 0;
+var pageContentEl = document.querySelector("#page-content");
 
 var TaskFormHandler = function(event) {
     event.preventDefault();
@@ -91,3 +92,19 @@ var createTaskActions = function(taskId) {
 
 formEl.addEventListener("submit", TaskFormHandler);
 
+var taskButtonHandler = function(event) {
+    console.log(event.target);
+
+    if(event.target.matches(".delete-btn")) {
+        //get the element's task id
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+};
+
+var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+  };
+
+pageContentEl.addEventListener("click", taskButtonHandler);
